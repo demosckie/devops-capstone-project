966caeb407f397d20568218351afa9c5293dd5b9
demosckie <vivant93@gmail.com>
2023-02-10 11:18:26 -0500

    added link task

--- service/routes.py
+++ service/routes.py
@@ -65,7 +65,7 @@ def create_accounts():
 # ... place you code here to LIST accounts ...
 
 
-@app.route("/accounts", methods=["GET"])  
+@app.route("/accounts", methods=["GET"])
 def list_accounts():
     """
     List all Accounts
@@ -84,6 +84,7 @@ def list_accounts():
 ######################################################################
 
 # ... place you code here to READ an account ...
+
 
 @app.route("/accounts/<int:account_id>", methods=["GET"])
 def get_accounts(account_id):
@@ -104,6 +105,7 @@ def get_accounts(account_id):
 ######################################################################
 
 # ... place you code here to UPDATE an account ...
+
 
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_accounts(account_id):
@@ -127,6 +129,7 @@ def update_accounts(account_id):
 ######################################################################
 
 # ... place you code here to DELETE an account ...
+
 
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
 def delete_accounts(account_id):
